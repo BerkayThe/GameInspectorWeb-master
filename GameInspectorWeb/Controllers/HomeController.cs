a4adc5256b3cae7cc97ca9d6c46494c25270bf3e
BerkayThe <74172777+BerkayThe@users.noreply.github.com>
2021-02-18 17:38:26 +0300

    Scrolling Page

--- GameInspectorWeb/Controllers/HomeController.cs
+++ GameInspectorWeb/Controllers/HomeController.cs
@@ -16,18 +16,19 @@ namespace GameInspectorWeb.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger,ApplicationDbContext applicationDbContext)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext applicationDbContext)
         {
             _logger = logger;
             _db = applicationDbContext;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int pageIndex = 0, int pageSize = 6)
         {
             ViewBag.Kategori = _db.Categories.Select(x => x.CategoryName).ToList();
 
-            var vm = _db.Articles.ToList();
+            ViewBag.Art = _db.Articles.OrderByDescending(x => x.Time).Take(5).ToList();
 
+            var vm = _db.Articles.OrderByDescending(x => x.Time).ToList();
             return View(vm);
         }
 
@@ -40,6 +41,15 @@ namespace GameInspectorWeb.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public JsonResult GetData(int pageIndex = 0, int pageSize = 6)
+        {
+            var vm = _db.Articles.OrderByDescending(x => x.Time).Skip(pageIndex * pageSize).Take(pageSize).ToList();
+
+            return Json(vm.ToList());
+            //return View(vm);
         }
     }
 }
